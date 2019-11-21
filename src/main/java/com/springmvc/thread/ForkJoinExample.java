19be62460eb685eafee736eacc6115588b3a40a9
tianyalei <tianyaleimessage@gmail.com>
2019-11-21 16:23:20 +0800

    add ForkJoin

---
+++ src/main/java/com/springmvc/thread/ForkJoinExample.java
@@ -0,0 +1,38 @@
+package com.springmvc.thread;
+
+
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * @author tianyalei
+ */
+public class ForkJoinExample extends RecursiveTask<Integer> {
+    private final int threshold = 5;
+    private int first;
+    private int last;
+
+    public ForkJoinExample(int first, int last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    protected Integer compute() {
+        int result = 0;
+        if (last - first <= threshold) {
+            // 任务足够小则直接计算
+            for (int i = first; i <= last; i++) {
+                result += i;
+            }
+        } else {
+            // 拆分成小任务
+            int middle = first + (last - first) / 2;
+            ForkJoinExample leftTask = new ForkJoinExample(first, middle);
+            ForkJoinExample rightTask = new ForkJoinExample(middle + 1, last);
+            leftTask.fork();
+            rightTask.fork();
+            result = leftTask.join() + rightTask.join();
+        }
+        return result;
+    }
+}
