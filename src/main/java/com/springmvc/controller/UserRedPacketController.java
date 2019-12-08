6e95e58f5c1332d58286bb41bad057370989b994
tianyalei <tianyaleimessage@gmail.com>
2019-12-09 06:48:56 +0800

    add UserRedPacketController

---
+++ src/main/java/com/springmvc/controller/UserRedPacketController.java
@@ -0,0 +1,33 @@
+package com.springmvc.controller;
+
+import com.springmvc.service.UserRedPacketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author tianyalei
+ */
+@Controller
+@RequestMapping("/userRedPacket")
+public class UserRedPacketController {
+
+    @Autowired
+    private UserRedPacketService userRedPacketService;
+
+    @RequestMapping("/grapRedPacket")
+    @ResponseBody
+    public Map<String, Object> grapRedPacket(Long redPacketId, Long userId) {
+        // 抢红包
+        int result = userRedPacketService.grapRedPacket( redPacketId, userId );
+        Map<String, Object> retMap = new HashMap<String, Object>();
+        boolean flag = result > 0;
+        retMap.put( "success", flag );
+        retMap.put( "message", flag ? "抢红包成功" : "抢红包失败" );
+        return retMap;
+    }
+}
