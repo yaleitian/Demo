284ce0bfc08ecadf011f25b4035c09bb545761a9
tianyalei <tianyaleimessage@gmail.com>
2020-04-18 10:26:08 +0800

    add ListTest.java

---
+++ src/main/java/test/ListTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+    List<String> stringList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ListTest listTest = new ListTest();
+        listTest.setStringList();
+    }
+    public void setStringList() {
+        stringList.add( "111" );
+        stringList.add( "222" );
+        stringList.add( "333" );
+        stringList.add( "444" );
+        for (String list: stringList){
+            System.out.println(list);
+        }
+    }
+}
